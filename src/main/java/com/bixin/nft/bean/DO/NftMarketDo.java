48fa44e5fd052a43e8a31b7875c2b05b901da546
zhangcheng <472012214@qq.com>
2021-09-23 16:08:17 +0800

    token

--- src/main/java/com/bixin/nft/bean/DO/NftMarketDo.java
+++ src/main/java/com/bixin/nft/bean/DO/NftMarketDo.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -15,7 +16,7 @@ import java.math.BigDecimal;
 * @created: 2021-09-18
 */
 @Data
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class NftMarketDo implements Serializable {
---
+++ src/main/java/com/bixin/nft/bean/vo/NftSelfSellingVo.java
@@ -0,0 +1,26 @@
+package com.bixin.nft.bean.vo;
+
+import com.bixin.nft.bean.DO.NftMarketDo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author zhangcheng
+ * create   2021/9/23
+ */
+@Data
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class NftSelfSellingVo extends NftMarketDo {
+
+    private String boxToken;
+    private String payToken;
+    private String nftMeta;
+    private String nftBody;
+
+}
