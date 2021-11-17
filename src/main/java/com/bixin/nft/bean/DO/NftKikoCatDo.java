b3a5ce65d72e16539a33ade8470f67560da6f325
xiangfeihan <xiangfeihan@bixin.com>
2021-11-17 18:39:45 +0800

    NFT增加属性

--- src/main/java/com/bixin/nft/bean/DO/NftKikoCatDo.java
+++ src/main/java/com/bixin/nft/bean/DO/NftKikoCatDo.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 * @Class: NftKikoCatDo
 * @Description: NFT Kiko猫信息表
 * @author: 系统
-* @created: 2021-09-17
+* @created: 2021-11-17
 */
 @Data
 public class NftKikoCatDo implements Serializable {
@@ -94,12 +94,342 @@ public class NftKikoCatDo implements Serializable {
     private BigDecimal eyesScore;
 
     /**
-     * 创建时间
+     * 帽子
+     */
+    private String hat;
+
+    /**
+     * 帽子分
+     */
+    private BigDecimal hatScore;
+
+    /**
+     * 服装
+     */
+    private String costume;
+
+    /**
+     * 服装分
+     */
+    private BigDecimal costumeScore;
+
+    /**
+     * 妆容
+     */
+    private String makeup;
+
+    /**
+     * 妆容分
+     */
+    private BigDecimal makeupScore;
+
+    /**
+     * 鞋子
+     */
+    private String shoes;
+
+    /**
+     * 鞋子分
+     */
+    private BigDecimal shoesScore;
+
+    /**
+     * 嘴
+     */
+    private String mouth;
+
+    /**
+     * 嘴分
+     */
+    private BigDecimal mouthScore;
+
+    /**
+     * 耳环
+     */
+    private String earring;
+
+    /**
+     * 耳环分
+     */
+    private BigDecimal earringScore;
+
+    /**
+     * 项链
+     */
+    private String necklace;
+
+    /**
+     * 项链分
+     */
+    private BigDecimal necklaceScore;
+
+    /**
+     * 颈部
+     */
+    private String neck;
+
+    /**
+     * 颈部分
+     */
+    private BigDecimal neckScore;
+
+    /**
+     * 头发
+     */
+    private String hair;
+
+    /**
+     * 头发分
+     */
+    private BigDecimal hairScore;
+
+    /**
+     * 角
+     */
+    private String horn;
+
+    /**
+     * 角分
+     */
+    private BigDecimal hornScore;
+
+    /**
+     * 手
+     */
+    private String hands;
+
+    /**
+     * 手分
+     */
+    private BigDecimal handsScore;
+
+    /**
+     * 身体
+     */
+    private String body;
+
+    /**
+     * 身体分
+     */
+    private BigDecimal bodyScore;
+
+    /**
+     * 皮肤
+     */
+    private String skin;
+
+    /**
+     * 皮肤分
+     */
+    private BigDecimal skinScore;
+
+    /**
+     * 纹身
+     */
+    private String tattoo;
+
+    /**
+     * 纹身分
+     */
+    private BigDecimal tattooScore;
+
+    /**
+     * 人物
+     */
+    private String people;
+
+    /**
+     * 人物分
+     */
+    private BigDecimal peopleScore;
+
+    /**
+     * 性格
+     */
+    private String characteristic;
+
+    /**
+     * 性格分
+     */
+    private BigDecimal characteristicScore;
+
+    /**
+     * 爱好
+     */
+    private String hobby;
+
+    /**
+     * 爱好分
+     */
+    private BigDecimal hobbyScore;
+
+    /**
+     * 星座
+     */
+    private String zodiac;
+
+    /**
+     * 星座分
+     */
+    private BigDecimal zodiacScore;
+
+    /**
+     * 动作
+     */
+    private String action;
+
+    /**
+     * 动作分
+     */
+    private BigDecimal actionScore;
+
+    /**
+     * 玩具
+     */
+    private String toys;
+
+    /**
+     * 玩具分
+     */
+    private BigDecimal toysScore;
+
+    /**
+     * 水果
+     */
+    private String fruits;
+
+    /**
+     * 水果分
+     */
+    private BigDecimal fruitsScore;
+
+    /**
+     * 蔬菜
+     */
+    private String vegetables;
+
+    /**
+     * 蔬菜分
+     */
+    private BigDecimal vegetablesScore;
+
+    /**
+     * 肉类
+     */
+    private String meat;
+
+    /**
+     * 肉类分
+     */
+    private BigDecimal meatScore;
+
+    /**
+     * 饮料
+     */
+    private String beverages;
+
+    /**
+     * 饮料分
+     */
+    private BigDecimal beveragesScore;
+
+    /**
+     * 食物
+     */
+    private String food;
+
+    /**
+     * 食物分
+     */
+    private BigDecimal foodScore;
+
+    /**
+     * 交通工具
+     */
+    private String vehicle;
+
+    /**
+     * 交通工具分
+     */
+    private BigDecimal vehicleScore;
+
+    /**
+     * 天气
+     */
+    private String weather;
+
+    /**
+     * 天气分
+     */
+    private BigDecimal weatherScore;
+
+    /**
+     * 月份
+     */
+    private String month;
+
+    /**
+     * 月份分
+     */
+    private BigDecimal monthScore;
+
+    /**
+     * 运动
+     */
+    private String sports;
+
+    /**
+     * 运动分
+     */
+    private BigDecimal sportsScore;
+
+    /**
+     * 音乐
+     */
+    private String music;
+
+    /**
+     * 音乐分
+     */
+    private BigDecimal musicScore;
+
+    /**
+     * 电影
+     */
+    private String movies;
+
+    /**
+     * 电影分
+     */
+    private BigDecimal moviesScore;
+
+    /**
+     * 季节
+     */
+    private String season;
+
+    /**
+     * 季节分
+     */
+    private BigDecimal seasonScore;
+
+    /**
+     * 搭配
+     */
+    private String outfit;
+
+    /**
+     * 搭配分
+     */
+    private BigDecimal outfitScore;
+
+    /**
+     * 
      */
     private Long createTime;
 
     /**
-     * 更新时间
+     * 
      */
     private Long updateTime;
 
